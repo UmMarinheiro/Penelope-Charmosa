b45cb3091007af5ccee29c481da7d163c23178c2
UmMarinheiro <souleonardo1@gmail.com>
2025-04-30 22:45:44 -0300

    Adicionei as portas

--- Penelope.ino
+++ Penelope.ino
@@ -1,9 +1,19 @@
-
-
 #define IN1 4
 #define IN2 5
 #define IN3 6
 #define IN4 7
+
+#define ENA 10
+#define ENB 11
+
+#define trigA 12
+#define echoA 13
+#define trigB 8
+#define echoB 9
+
+#define sensorA A3
+#define sensorB A2
+#define sensorT A1
 
 void setup() {
   // put your setup code here, to run once:
@@ -11,6 +21,22 @@ void setup() {
   pinMode(IN2,OUTPUT);
   pinMode(IN3,OUTPUT);
   pinMode(IN4,OUTPUT);
+
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+
+  pinMode(trigA, OUTPUT);
+  pinMode(echoA, INPUT);
+  pinMode(trigB, OUTPUT);
+  pinMode(echoB, INPUT);
+
+  pinMode(sensorA, INPUT);
+  pinMode(sensorB, INPUT);
+  pinMode(sensorT, INPUT);
+  
+
+  analogWrite(ENA,200);
+  analogWrite(ENB,200);
 }
 void back()
 {
@@ -33,12 +59,8 @@ void stop()
   digitalWrite(IN3,LOW);
   digitalWrite(IN4,LOW);
 }
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
-  back();
-  delay(1000);
   forward();
-  delay(1000);
-  stop();
-  delay(1000);
 }
